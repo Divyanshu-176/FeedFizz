9711f49f4ef4201baa27d1b6c8354d33d47aeb97
Divyanshu-176 <divyanshujain176@gmail.com>
2026-03-28 14:01:49 +0000

    ...

--- app/feedback/new/page.tsx
+++ app/feedback/new/page.tsx
@@ -1,3 +1,29 @@
+import { Button } from "@/components/ui/button";
+import { Card, CardContent, CardDescription, CardHeader, CardTitle } from "@/components/ui/card";
+import { ArrowLeft } from "lucide-react";
+import Link from "next/link";
+
 export default async function NewFeedbackPage(){
-    return <>newFeedback</>
+    return <>
+        <div className="max-w-2xl mx-auto space-y-6">
+            <div className="flex items-center gap-2">
+                <Button variant="ghost" size="icon" asChild >
+                    <Link href="/feedback">
+                        <ArrowLeft className="h-4 w-4"/>
+                    </Link>
+                </Button>
+                <h1 className="text-3xl font-bold">Share your feedback</h1>
+            </div>
+
+            <Card>
+                <CardHeader>
+                    <CardTitle> New Feedback</CardTitle>
+                    <CardDescription>Share your idea with the community</CardDescription>
+                    <CardContent>
+                        
+                    </CardContent>
+                </CardHeader>
+            </Card>
+        </div>
+    </>
 }
