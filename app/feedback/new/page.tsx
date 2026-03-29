24ef340fc232b5fd8762c8786a14f66a08c45a55
Divyanshu-176 <divyanshujain176@gmail.com>
2026-03-29 11:08:39 +0000

    new feedback page

--- app/feedback/new/page.tsx
+++ app/feedback/new/page.tsx
@@ -1,9 +1,86 @@
+"use client"
+
+import { CATEGORIES_TYPES } from "@/app/api/data/category-data";
 import { Button } from "@/components/ui/button";
 import { Card, CardContent, CardDescription, CardHeader, CardTitle } from "@/components/ui/card";
+import { Input } from "@/components/ui/input";
+import { Label } from "@/components/ui/label";
+import { Textarea } from "@/components/ui/textarea";
 import { ArrowLeft } from "lucide-react";
 import Link from "next/link";
+import { useRouter } from "next/navigation";
+import { useActionState, useEffect } from "react";
+import { toast } from "sonner";
 
-export default async function NewFeedbackPage(){
+
+
+
+async function submitFeedback(prevState:{success:boolean, error:string}, formData:FormData){
+    
+    
+    const loadingToast = toast.loading("Submitting your feedback")
+
+    try {
+        const response = await fetch("/api/feedback",{
+            method:"POST",
+            headers:{
+                "Content-type":"application/json"
+            },
+            body:JSON.stringify({
+                title:formData.get("title"),
+                description:formData.get("description"),
+                category:formData.get("category")
+            })
+        })
+
+        if(!response.ok){
+            throw new Error("Failed to create post")
+        }
+
+        toast.dismiss(loadingToast)
+        toast.success("Your feedback has been submitted successfully")
+
+
+        return {
+            success:true,
+            error:""
+        }
+
+
+        
+    } catch (error) {
+        console.error("Something went wrong: ",error)
+        toast.dismiss(loadingToast)
+        toast.error("Something Went wrong")
+
+        return {
+            success:false,
+            error:"Failed to submit"
+        }
+    }
+}
+
+
+
+export default function NewFeedbackPage(){
+    toast.info("info")
+
+    const router = useRouter()
+
+    const [state, action, isPending] = useActionState(submitFeedback, {
+        success:false,
+        error:""
+    })
+
+    useEffect(()=>{
+        if(state.success){
+            const timer = setTimeout(()=>{router.push("/feedback"); router.refresh()},500)
+            
+            return ()=>clearTimeout(timer)
+        }
+    },[state.success, router])
+    
+
     return <>
         <div className="max-w-2xl mx-auto space-y-6">
             <div className="flex items-center gap-2">
@@ -19,10 +96,35 @@ export default async function NewFeedbackPage(){
                 <CardHeader>
                     <CardTitle> New Feedback</CardTitle>
                     <CardDescription>Share your idea with the community</CardDescription>
-                    <CardContent>
-                        
-                    </CardContent>
                 </CardHeader>
+                <CardContent>
+                    <form action={action} className="space-y-6">
+                        <div className="space-y-2">
+                            <Label htmlFor="title" >Title</Label>
+                            <Input id="title" name="title" placeholder="What would you like to see?" required />
+                        </div>
+                        <div className="space-y-2">
+                            <Label htmlFor="category" >Category</Label>
+                            <select id="category" name="category" className="w-full px-3 py-2 border rounded-md bg-background" defaultValue={CATEGORIES_TYPES[0]}>
+                                {CATEGORIES_TYPES.map((category)=>(
+                                    <option key={category} value={category}>{" "}{category}</option>
+                                ))}
+                            </select>
+                        </div>
+                        <div className="space-y-2">
+                            <Label htmlFor="description" >Description</Label>
+                            <Textarea id="description" name="description" placeholder="Describe your idea in detail" required />
+                        </div>
+                        <div className="flex gap-4">
+                            <Button type="submit" disabled={isPending}>{isPending? "Submitting" : "Submit Feedback"}</Button>
+                            <Button type="button" variant={"outline"} asChild>
+                                <Link href="/feedback">
+                                    Cancel
+                                </Link>
+                            </Button>
+                        </div>
+                    </form>
+                </CardContent>
             </Card>
         </div>
     </>
--- app/layout.tsx
+++ app/layout.tsx
@@ -31,7 +31,7 @@ export default async function RootLayout({
               {children}
             </main>
             <Footer/>
-            <Toaster/>
+            <Toaster richColors/>
           </ThemeProvider>
         </body>
       </html>
